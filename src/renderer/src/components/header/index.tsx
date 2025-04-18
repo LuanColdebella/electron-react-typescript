39e20ed810baa01a696ef46a5081dec911fbabb5
LuanColdebella <luanarabuta@gmail.com>
2025-04-17 23:34:39 -0300

    feat: update @radix-ui/react-collapsible to v1.1.4 and refactor routing structure with Layout component

---
+++ src/renderer/src/components/header/index.tsx
@@ -0,0 +1,9 @@
+import * as Collapsible from '@radix-ui/react-collapsible';  
+
+export function Header() {
+    return (
+        <Collapsible.Root>
+            <h1>Header</h1>
+        </Collapsible.Root>
+    )
+}
--- src/renderer/src/pages/home.tsx
+++ src/renderer/src/pages/home.tsx
@@ -1,8 +1,11 @@
+import { Link} from 'react-router-dom'
+
 export function Home () {
     return (
     <div>
     <h1>Página HOME!!!</h1>
     <h3>ETSTESTETSTEST</h3>
+    <Link to='/create'>ir para a página create</Link>
     </div>
     )
 }
