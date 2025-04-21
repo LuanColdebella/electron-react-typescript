bdac83346303cf7e6e8cce610db7de9f5722c27a
LuanColdebella <luanarabuta@gmail.com>
2025-04-21 16:10:16 -0300

    feat: add region-drag utility for draggable areas and update header and sidebar components

--- src/renderer/src/components/sidebar/index.tsx
+++ src/renderer/src/components/sidebar/index.tsx
@@ -24,6 +24,14 @@ export function Sidebar(){
         <ArrowBendDoubleUpLeft className='h-7 w-7'/>
       </Collapsible.Trigger>
 
+      <div
+        className={clsx('regrion-drag h-14 z-0 mt-10', {
+          block: isMacOS,
+          hidden: !isMacOS
+        })}
+      >
+      </div>
+
       <div 
         className={clsx(
           'flex-1 flex flex-col h-full gap-8 w-[220px] transition-opacity group-data-[state=open]:opacity-100 group-data-[state=closed]:opacity-0 duration-200',
